02b5209bcb81c268da2c795a66344415a3d94bac
mshahzebraza97 <m.shahzebraza.97@gmail.com>
2022-01-22 22:00:10 +0500

    loading transactions dynamically from MWOs & POs

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import '../styles/globals.scss';
-import { Provider } from 'react-redux';
+import { Provider, useDispatch } from 'react-redux';
 import { store } from '../store/index'
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
 function MyApp({ Component, pageProps }) {
+
+
   return (
     <Provider store={store} >
       <div id="portalRoot"></div>

6e928991f89f4aece0c5cde478511309baf8a895
mshahzebraza97 <m.shahzebraza.97@gmail.com>
2022-02-03 19:08:29 +0500

    learnt something

---
+++ notes/bugs/asyncFnReturnsPromise.md
@@ -0,0 +1,5 @@
+Trying to return a value from async function always shows a pending promise as returned value.
+[Same Question asked on Stack Overflow>>](https://stackoverflow.com/questions/51338277/async-function-returning-promise-instead-of-value)
+
+**Brief answer:**
+Async prefix is a kind of wrapper for Promises. and always returns a promise.
--- pages/_app.js
+++ pages/_app.js
@@ -5,18 +5,14 @@ import '../styles/globals.scss';
 
 import { ApolloProvider } from "@apollo/client";
 import client from '../lib/apollo_client/index';
-import poApollo from '../lib/apollo_client/poApollo';
+import poApollo, { fetchAllPO } from '../lib/apollo_client/poApollo';
 import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
 
   useEffect(() => {
-    (async () => {
-      // Request data for all slices from a single endpoint
-      const res = await fetch('http://localhost:3000/api/po');
-      const resJSON = await res.json();
-      poApollo(resJSON.data)
-    })()
+
+    fetchAllPO()
 
   }, []);
 

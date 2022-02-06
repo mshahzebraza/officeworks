977648ffc0e5fa90b735793deff3014c1c0d343c
mshahzebraza97 <m.shahzebraza.97@gmail.com>
2022-02-07 00:17:00 +0500

    mwo Schema Created

---
+++ server/models/mwoModel.js
@@ -0,0 +1,18 @@
+// import { Mongoose as mongoose } from "mongoose";
+const mongoose = require('mongoose');
+
+const mgSchema = mongoose.Schema;
+
+const mwoSchema = new mgSchema({
+  mwoId: { type: String, unique: true },
+  application: String,
+  itemId: String,
+  itemName: String,
+  qty: Number,
+  title: String,
+  status: String,
+  remarks: String,
+})
+
+const modal_MWO = mongoose.models.MWO || mongoose.model('MWO', mwoSchema);
+export default modal_MWO;

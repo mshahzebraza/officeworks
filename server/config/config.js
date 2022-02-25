a7128c92938ed9376f7c14eabcddc68ba0f07592
mshahzebraza97 <m.shahzebraza.97@gmail.com>
2022-02-25 20:19:48 +0500

    minor

--- server/config/config.js
+++ server/config/config.js
@@ -8,38 +8,36 @@ import CatchAsyncErrors from '../middlewares/CatchAsyncErrors';
 
 const connectDB = CatchAsyncErrors(async () => {
 
-  // try {
-
   // Method 01
-  const connection = mongoose.connect(
+  mongoose.connect(
     process.env.MONGO_URI
     , function () { console.log('Success: connected to MongoDB') }
     , function () { console.log('Error connecting to MongoDB') }
   )
 
+
   // Method 02
   /* 
-    const connection = await mongoose.createConnection(
+    mongoose.createConnection(
       process.env.MONGO_URI // 'mongodb://localhost:27017/OfficeWorks',
       , { useNewUrlParser: true, useUnifiedTopology: true }
     )
     console.log(`Connected to MongoDB @ ${mongoose.connection.host}`);
   
-    connection.on('connected', function () {
+    // mongoose successful connection message
+    mongoose.connection.on('connected', function () {
+      console.log('Mongoose default connection open to ' + process.env.MONGO_URI)
       console.log('Success: connected to MongoDB');
     })
-    connection.on('error', function () {
+  
+    // If the connection throws an error 
+    mongoose.connection.on('error', function (err) {
+      console.log('MongoDB connection error: ' + err)
       console.log('Error connecting to MongoDB');
-      process.exit(1)
-    }) 
-    */
+      process.exit(-1)
+    })
+   */
 
-
-  return mongoose.Connection;
-
-  // } catch (error) {
-  //   console.log(`DB Error ${error}`);
-  // };
 });
 
 // module.exports = connectDB; // OR export default connectDb;

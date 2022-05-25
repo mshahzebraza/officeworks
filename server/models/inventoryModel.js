69725db7abf7fbc7029cedde9f160f14d815b9bb
mshahzebraza97 <m.shahzebraza.97@gmail.com>
2022-05-25 16:35:29 +0500

    minor: removal of unneeded files

--- server/models/inventoryModel.js
+++
@@ -1,6 +0,0 @@
-// Product Nomenclature (with Alias if any, to match with project modules)
-// Product ID
-// Qty
-// Project Application(s) - This may need translator function - connection with Projects required
-// Specs of Product - Cascaded (for multiple versions of same ID) and nested - alias of different versions matching with different project modules.
-// Batches - clickable (A List of Transactions which are made specifically by the system i.e. from POs and MWOs)

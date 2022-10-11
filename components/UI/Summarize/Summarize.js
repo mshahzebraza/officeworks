87d924df8985ce1a725f656286acd2e5654e920a
mshahzebraza97 <m.shahzebraza.97@gmail.com>
2022-10-11 22:50:42 +0500

    minor: component-refactoring

--- components/UI/Summarize/Summarize.js
+++ components/UI/Summarize/Summarize.js
@@ -1,57 +1,7 @@
-import { checkDataType, deepClone, replaceKeysMap, summarizer, summarizerNew2, toSentenceCase } from "../../../helpers/reusable";
-import DetailItem from "../../Detail&Summary/DetailItem";
 import styles from './Summarize.module.scss'
+import { checkDataType, deepClone, summarizerNew2, toSentenceCase } from "../../../helpers/reusable";
+import { SummaryItem } from "./SummaryItem";
 
-
-// to render item: {qty,item} structures
-function RepeatedItemJSX({ item }) {
-    if (!item) return <span>No items</span>;
-    return (<li className={styles.dataItem}>
-        <span className={styles.dataItemQty}> {item.qty} </span>
-        <span className={styles.dataItemType}>{item.item}</span>
-    </li>);
-}
-
-function SummaryItem({ field = 'noField', value = 'noValue', isList = false }) {
-
-    // Check for List (Array)
-    if (isList) {
-        if (value?.length > 0) {
-            value = value.map(
-                (item, idx) =>
-                    <RepeatedItemJSX
-                        key={idx}
-                        item={item}
-                    />
-            )
-        } else {
-            value = <RepeatedItemJSX />
-        }
-    }
-
-    // Check for empty strings
-    value = value === '' ? '---' : value;
-
-
-
-    return (
-        <DetailItem>
-
-            <div className={styles.data}>
-                {/* Place Keys in span */}
-                <span className={styles.dataField}>{field}:</span>
-                {
-                    // Check for list items
-                    !isList
-                        // Place strings in span
-                        ? <span className={styles.dataValue}>{value}</span>
-                        // Place list items in ul
-                        : <ul className={styles.dataValue}>{value}</ul>
-                }
-            </div>
-        </DetailItem>
-    );
-}
 
 export function Summarize(
     {

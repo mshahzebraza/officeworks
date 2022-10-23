185c456b2be21c1b91828b7253f42776bd3c73bd
mshahzebraza97 <m.shahzebraza.97@gmail.com>
2022-10-23 13:02:06 +0500

    minor: adds formatting changes. resolves #14 : updates po summary view

--- components/PO/PO_Summary/index.js
+++ components/PO/PO_Summary/index.js
@@ -16,7 +16,7 @@ export default function PO_Summary({
     handleClose: modalCloser,
     data: poData,
     // the default config for PO-schema
-    config = defaultPOsummaryConfig,
+    config = getDefaultPOsummaryConfig({ currency: poData.currency }),
     viewRawData = false
 
 }) {
@@ -47,32 +47,55 @@ export default function PO_Summary({
     )
 }
 
-const defaultPOsummaryConfig = {
-    // new config ... 
-    deleteKeys: ['id', '_id', '__v', 'index', 'tableData'],
-    renameKeys: {
-        refType: 'Reference Type',
-        refId: 'Reference ID',
-        category: 'PO Category',
-        fulfillmentSource: 'Fulfillment Source',
-        currency: 'Currency',
-        totalCost: 'Total Cost',
-        status: 'Status',
-        initiatorId: 'Initiator ID',
-        supplier: 'Supplier',
-        items: 'Procured Modules',
-        remarks: 'Remarks',
-        id: 'ID',
-    },
-    render: {
-        default: ({ label, value }) => <SummaryItem label={label} value={value} />,
-        items: ({ label, value }) => <SummaryItem label={label} value={<SummaryItemValue_Items value={value} />} />,
-        status: ({ label, value }) => <SummaryItem label={label} value={`phase-${value}`} />,
-    }
-    // pullNested: []
+function getDefaultPOsummaryConfig({ currency }) {
+
+    return ({
+        // new config ... 
+        deleteKeys: ['id', '_id', '__v', 'index', 'tableData', 'currency'],
+        renameKeys: {
+            refType: 'Reference Type',
+            refId: 'Reference ID',
+            category: 'PO Category',
+            fulfillmentSource: 'Fulfillment Source',
+            currency: 'Currency',
+            totalCost: 'Total Cost',
+            status: 'Status',
+            initiatorId: 'Initiator ID',
+            supplier: 'Supplier',
+            items: 'Procured Modules',
+            remarks: 'Remarks',
+            id: 'ID',
+        },
+        render: {
+            default: ({ label, value }) => <SummaryItem label={label} value={value} />,
+            items: ({ label, value }) => <SummaryItem label={label} value={<SummaryItemValue_Items value={value} currency={currency} />} />,
+            status: ({ label, value }) => <SummaryItem label={label} value={`phase-${value}`} />,
+            totalCost: ({ label, value }) => <SummaryItem label={label} value={`${currency} ${value}`} />,
+        }
+        // pullNested: []
+    })
 }
 
-export function SummaryItemValue_Items({ value: AoOdata }) {
+const myModuleList = [
+    { type: 'motor', id: 'J52SY350A' },
+    { type: 'motor', id: 'J64SY700C' },
+    { type: 'screw', id: 'M5x12' },
+    { type: 'potentiometer', id: 'MSL-130-100-S-N' },
+    { type: 'motor', id: 'Oipiisda-121' },
+]
+
+function getModuleType(AoOdata, moduleList) {
+    return AoOdata.map(
+        (myObj, idx) => {
+            const matchingModule = moduleList.find((module) => module.id === myObj.id);
+            myObj.type = matchingModule?.type ?? '-';
+            return myObj;
+        }
+    );
+}
+
+function SummaryItemValue_Items({ value: AoOdata, currency }) {
+    AoOdata = getModuleType(AoOdata, myModuleList)
     const dataHeaders = Object.keys(AoOdata[0])
     const propertyConfig = {
         id: {
@@ -83,13 +106,17 @@ export function SummaryItemValue_Items({ value: AoOdata }) {
             span: 2,
             label: 'Quantity'
         },
+        type: {
+            span: 2,
+            label: 'Type'
+        },
         unitPrice: {
             span: 2,
             label: 'Unit Price',
-            render: (price) => `${price}/- XXX`
+            transformer: (price) => `${currency} ${price}`
         },
         remarks: {
-            span: 6,
+            span: 4,
             label: 'Remarks'
         },
     }
@@ -125,6 +152,9 @@ export function SummaryItemValue_Items({ value: AoOdata }) {
                                 dataEntries.map(([dataKey, dataValue], idx) => {
                                     const entryConfig = propertyConfig[dataKey];
                                     const entryWidth = entryConfig?.span || 2
+                                    const entryTransformer = entryConfig?.transformer
+                                    if (entryTransformer) dataValue = entryTransformer(dataValue)
+
                                     return (
                                         <Grid key={idx} item xs={entryWidth}
                                             sx={{ p: 1, border: '1px dashed black' }}
--- helpers/summarizer.js
+++ helpers/summarizer.js
@@ -68,14 +68,6 @@ function getAdvDataType(data) {
 
 
 
-
-const myModuleList = [
-    { type: 'motor', id: 'J52SY350A' },
-    { type: 'motor', id: 'J64SY700C' },
-    { type: 'screw', id: 'M5x12' },
-    { type: 'potentiometer', id: 'MSL-130-100-S-N' },
-]
-
 /**
  * Received:
  * {

8e7763c6c3d9fa70110c54664d6b2e52a43866a3
mshahzebraza97 <m.shahzebraza.97@gmail.com>
2022-11-02 21:15:24 +0500

    feat: updated model of MWO

--- server/models/mwoModel.js
+++ server/models/mwoModel.js
@@ -5,22 +5,23 @@ const Schema = mongoose.Schema;
 const ObjectId = Schema.Types.ObjectId;
 
 const mwoSchema = new Schema({
+    mwoType: String,
     mwoId: { type: String, unique: true },
     title: String,
     status: { type: Number, default: 0 },
-    initiatorId: { type: ObjectId, default: 0 },
+    initiatorId: { type: Number, default: 0 },
     remarks: String,
-    // supplier: PPC, MSS, etc
-
     items: [
         {
-            item: { type: ObjectId, ref: 'Module' },
+            // module: { type: ObjectId, ref: 'Module' },
+            id: String,
             qty: Number,
-            remarks: String,
+            remarks: String, // purchase remarks
             // no mongoose _id required for the document
             _id: false,
         }
-    ], // assuming only one item per mwo
+    ],
+    // supplier: PPC, MSS, etc
     // TODO: add other fields like MWO report, etc. (and the ones mentioned in the MWO report)
 })
 
